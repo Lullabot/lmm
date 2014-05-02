23cdea108f892a3df34f82149e157ffc0f7c1590
Andrew Berry <deviantintegral@gmail.com>
2014-05-02 15:11:35 -0400

    Move branching to a function.

--- branch-db.sh
+++ branch-db.sh
@@ -11,14 +11,4 @@ fi
 
 snapshot_available "$VG_PATH/$1"
 check_user
-
-LINK=`readlink /var/lib/mysql`
-if [ "$LINK" == "$VG_PATH/master" ]
-then
-  service mysql stop
-fi
-
-lvcreate -s -n $1 -L2G mysql/master
-mkdir -p $VG_PATH/$1
-mount /dev/mysql/$1 $VG_PATH/$1
-./switch-db.sh $1
+branch $1
